be07fc85e53e5e63575e78fed167432ffe2121b1
krish808 <krishna143309@gmail.com>
2026-03-10 00:24:37 +0530

    fix:correct products api route

--- apps/web/src/services/productService.js
+++ apps/web/src/services/productService.js
@@ -1,11 +1,11 @@
 import api from "./api";
 
 export const getAllProducts = async () => {
-  const res = await api.get("/products/all");
+  const res = await api.get("/api/products/all");
   return res.data;
 };
 
 export const getProductById = async (id) => {
-  const res = await api.get(`/products/${id}`);
+  const res = await api.get(`/api/products/${id}`);
   return res.data;
 };
